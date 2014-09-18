46249059b412d064c6ec371d17daa8ff54addc5d
Francoisp <francoisp@shuttle.(none)>
2014-09-18 15:39:58 +0200

    Added Theodo logo Configured for Capifony

---
+++ app/config/deploy.rb
@@ -0,0 +1,19 @@
+set   :application,   "Symfony Modifiée"
+set   :deploy_to,     "/var/www/Symfony"
+set   :domain,        "cloud-francoisp"
+set   :webserver_user,"www-data"
+
+set   :scm,           :git
+set   :repository,    "https://github.com/FrancoisPl/symfony-standard.git"
+
+role  :web,           domain
+role  :app,           domain, :primary => true
+
+set   :use_sudo,      false
+set   :keep_releases, 3
+set   :shared_files,  ["app/config/parameters.yml"]
+set   :shared_children,[app_path + "/logs", web_path + "/uploads", "vendor"]
+set   :use_composer,  true
+set   :update_vendors,true
+
+logger.level = Logger::MAX_LEVEL
