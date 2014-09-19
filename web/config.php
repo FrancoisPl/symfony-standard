196f0adf597a7e1e1ab05c7be136e63d40685de1
Francoisp <francoisp@shuttle.(none)>
2014-09-19 10:52:14 +0200

    Try

--- web/config.php
+++ web/config.php
@@ -26,7 +26,7 @@ $minorProblems = $symfonyRequirements->getFailedRecommendations();
         <div id="content">
             <div class="header clear-fix">
                 <div class="header-logo">
-                    <img src="bundles/framework/images/theodo-logo-medium.png" alt="Symfony" />
+                    <img src="bundles/framework/images/logo_symfony.png" alt="Symfony" />
                 </div>
 
                 <div class="search">
